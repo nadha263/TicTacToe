80c9a99baffe57ce5ca06f18952c701c04df8c16
nadha263 <sanoojmohamed75@gmail.com>
2026-04-21 14:45:18 +0530

    uc2

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.Random;
+
+public class TicTacToe {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+
+
+        int toss = random.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }}
+
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human won the toss!");
+            System.out.println("Human plays first with symbol: " + humanSymbol);
+            System.out.println("Computer gets symbol: " + computerSymbol);
+        } else {
+            System.out.println("Computer won the toss!");
+            System.out.println("Computer plays first with symbol: " + computerSymbol);
+            System.out.println("Human gets symbol: " + humanSymbol);
+        }
+    }}
